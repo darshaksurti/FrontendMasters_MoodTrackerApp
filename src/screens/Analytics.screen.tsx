45747f187682432b82823988f90546d63c44349c
Darshak Surti <dsurti@DSurti-MacBook-Pro.local>
2022-04-22 16:53:43 -0400

    Added analytics (pie chart)

--- src/screens/Analytics.screen.tsx
+++ src/screens/Analytics.screen.tsx
@@ -1,13 +1,27 @@
 import React from "react";
 import { View, Text } from "react-native";
-import { useAppContext } from "../App.provider";
+import { useAppContext } from '../App.provider';
+import { VictoryPie } from 'victory-native';
+import { groupBy } from "lodash";
 
 export const Analytics: React.FC = () => {
+    const appContext = useAppContext();
+
+    const data = Object.entries(groupBy(appContext.moodList, 'mood.emoji')).map(
+        ([key, value]) => ({
+            x: key,
+            y: value.length,
+        })
+    );
+
     return (
         <View>
             <Text>
                 Analytics
             </Text>
+            <VictoryPie
+                data={data}
+            />
         </View>
     )
 }

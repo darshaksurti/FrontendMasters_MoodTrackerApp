e4ebde11ff78f51f4af36b31078cf26ce16a6d73
Darshak Surti <dsurti@DSurti-MacBook-Pro.local>
2022-04-22 14:00:05 -0400

    Added fonts

--- src/screens/Home.screen.tsx
+++ src/screens/Home.screen.tsx
@@ -1,14 +1,25 @@
 import React from "react";
-import { View, StyleSheet } from "react-native";
+import { View, StyleSheet, Image, ImageBackground } from "react-native";
 import { MoodPicker } from "../components/MoodPicker";
 import { useAppContext } from "../App.provider";
+
+
+const imageUrl = 'https://images.unsplash.com/photo-1474540412665-1cdae210ae6b?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1766&q=80';
 
 export const Home: React.FC = () => {
     const AppContext = useAppContext();
     return (
-        <View style={styles.container}>
+        // <View style={styles.container}>
+        //     <Image source={{ uri: imageUrl }} style={styles.image} />
+        //     <View style={[StyleSheet.absoluteFill, styles.foreground]}>
+        //         <MoodPicker handleSelectMood={AppContext.handleSelectMood} />
+        //     </View>
+        // </View>
+
+        // You can also use the Image Background Tag to get the same results:
+        <ImageBackground style={styles.container} source={{ uri: imageUrl }}>
             <MoodPicker handleSelectMood={AppContext.handleSelectMood} />
-        </View>
+        </ImageBackground>
     )
 }
 
@@ -16,5 +27,11 @@ const styles = StyleSheet.create({
     container: {
         flex: 1,
         justifyContent: 'center',
-    }
+    },
+    image: {
+        flex: 1,
+    },
+    foreground: {
+        justifyContent: 'center',
+    },
 })
